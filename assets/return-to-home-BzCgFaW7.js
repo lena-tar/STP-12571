9466f8b79bbe2d77f1499d2e7092ed7e467e22a5
lena-tar <taran9933@gmail.com>
2026-05-09 22:31:10 +0000

    Deploying to gh-pages from @ lena-tar/STP-12571@b32d91a03d3e38aad7689349b864b1b0bf9a98be 🚀

---
+++ assets/return-to-home-BzCgFaW7.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#return-to-home");e&&e.addEventListener("click",()=>{window.location.href="/STP-12571/"});
+//# sourceMappingURL=return-to-home-BzCgFaW7.js.map
--- privacy-page.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector("#return-to-home");t&&t.addEventListener("click",()=>{window.location.href="/STP-12571/"});
-//# sourceMappingURL=privacy-page.js.map
